43490e86e35295cda2a16458b691da5b8596aa04
NguyenLV <nguyenlvce181858@fpt.edu.vn>
2026-05-31 23:51:55 +0700

    Add @JsonProperty mappings for SePay webhook snake_case properties

--- AppQuanLiChiTieu/AppQuanLiChiTieu/src/main/java/com/example/AppQuanLiChiTieu/dto/request/SepayWebhookRequest.java
+++ AppQuanLiChiTieu/AppQuanLiChiTieu/src/main/java/com/example/AppQuanLiChiTieu/dto/request/SepayWebhookRequest.java
@@ -1,5 +1,6 @@
 package com.example.AppQuanLiChiTieu.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -11,15 +12,32 @@ import lombok.experimental.FieldDefaults;
 public class SepayWebhookRequest {
     Long id;
     String gateway;
+
+    @JsonProperty("transaction_date")
     String transactionDate;
+
+    @JsonProperty("account_number")
     String accountNumber;
+
+    @JsonProperty("sub_account")
     String subAccount;
+
     Double amount;
+
+    @JsonProperty("transfer_type")
     String transferType;
+
+    @JsonProperty("transfer_amount")
     Double transferAmount;
+
     Double accumulated;
     String code;
+
+    @JsonProperty("transaction_content")
     String transactionContent;
+
+    @JsonProperty("reference_number")
     String referenceNumber;
+
     String body;
 }
